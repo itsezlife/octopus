1d07dfc99589f9c262343057da2397fb5cb0a642
Emil <emilzulufov.commercial@gmail.com>
2025-12-01 00:49:06 +0500

    feat: extend OctopusDialogPage with additional customization options
    - Added parameters for barrier color, dismissibility, label, safe area usage, focus request, anchor point, and traversal edge behavior.
    - Updated createRoute method to incorporate new parameters for enhanced dialog customization.

--- lib/src/widget/dialog_page.dart
+++ lib/src/widget/dialog_page.dart
@@ -10,14 +10,39 @@ class OctopusDialogPage extends Page<Object?> {
     super.name,
     super.arguments,
     super.restorationId,
+    this.themes,
+    this.barrierColor = Colors.black54,
+    this.barrierDismissible = true,
+    this.barrierLabel,
+    this.useSafeArea = true,
+    this.requestFocus,
+    this.anchorPoint,
+    this.traversalEdgeBehavior,
   });
 
   final WidgetBuilder builder;
+
+  final Color? barrierColor;
+  final bool barrierDismissible;
+  final String? barrierLabel;
+  final bool useSafeArea;
+  final CapturedThemes? themes;
+  final bool? requestFocus;
+  final Offset? anchorPoint;
+  final TraversalEdgeBehavior? traversalEdgeBehavior;
 
   @override
   Route<void> createRoute(BuildContext context) => DialogRoute(
         context: context,
         builder: builder,
+        themes: themes,
+        barrierColor: barrierColor,
+        barrierDismissible: barrierDismissible,
+        barrierLabel: barrierLabel,
+        useSafeArea: useSafeArea,
+        requestFocus: requestFocus,
+        anchorPoint: anchorPoint,
+        traversalEdgeBehavior: traversalEdgeBehavior,
         settings: this,
       );
 }
